1a252269d42b171f0a0ea56bbebbef67ca98d86c
Vlad <excelent91@mail.ru>
2019-04-22 22:36:12 +0300

    first commit

---
+++ pages/_error.js
@@ -0,0 +1,5 @@
+const Error = () => (
+	<div><h1>Error happend!</h1></div>
+);
+
+export default Error;
---
+++ pages/index.js
@@ -0,0 +1,5 @@
+const Index = () => (
+	<div><h1>Hello</h1></div>
+);
+
+export default Index;
